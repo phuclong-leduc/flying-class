6c003102c076416a88aca0f7b0d899cc8545ccea
Phuong Tran <phuong0968804740@gmail.com>
2026-03-17 17:07:37 +0700

    fix docker

--- flying-class-backend/prisma.config.ts
+++ flying-class-backend/prisma.config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.PRISMA_DATABASE_URL || process.env.DATABASE_URL,
-    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+    url:
+      process.env.PRISMA_DATABASE_URL ||
+      process.env.DATABASE_URL ||
+      "postgresql://postgres:postgres@localhost:5432/postgres?schema=public",
+    shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL,
   },
 });
